b1cbcfda09c190d0c46b856a3ab819cf270b18b0
Gustavo-GPG <gustavogpg23@gmail.com>
2026-02-03 16:18:53 -0300

    adição de produtos

--- src/data/products.js
+++ src/data/products.js
@@ -5,64 +5,73 @@ import creatina1kgImg from "../assets/creatina1kg.png";
 import creatina150Img from "../assets/crea150.png";
 import carneHorse from "../assets/carneHorse.png";
 import creaHorse from "../assets/creaHorse.png";
-import wheyBar from "../assets/wheyBar.png";
+import pumonewEB from "../assets/pumonewEB.png";
+import pumonew from "../assets/pumonew.png"
+import combo1 from "../assets/combo1.png"
 
 
 export const products = [
-  {
+    {
     id: 1,
+    name: "COMBO NEW GOLD HORSE 907G + PUMONEW 300G + CREATINA 100% PURA 300G - VITAMIN HORSE",
+    image: combo1,
+    price: 379.90,
+    url: "https://vitaminhorse.com.br/produtos/combo-new-gold-horse-907g-pumonew-300g-creatina-100-pura-300g-vitamin-horse/"
+  },
+  {
+    id: 2,
+    name: "PRÉ-TREINO PUMONEW 300g - VITAMIN HORSE",
+    image: pumonew,
+    price: 149.90,
+    url: "https://vitaminhorse.com.br/produtos/pre-treino-pumonew-300g-vitamin-horse/"
+  },
+  {
+    id: 3,
     name: " CREATINA 100% PURA 300g - VITAMIN HORSE",
     image: creatinaImg,
     price: 59.90,
     url: "https://vitaminhorse.com.br/produtos/creatina-100-pura-300g-vitamin-horse/"
   },
   {
-    id: 2,
+    id: 4,
     name: "Whey 100% Vitamin Horse 1kg 24g Proteína + Creatina",
     image: wheyImg,
     price: 149.90,
     url: "https://vitaminhorse.com.br/produtos/whey-100-vitamin-horse-1kg-24g-proteina-creatina-kdpac/"
   },
   {
-    id: 3,
+    id: 5,
     name: "HORSE CREAM 200g SABOR AVELÃ COM PIMENTA E CREATINA - VITAMIN HORSE",
     image: cremeImg,
     price: 59.90,
     url: "https://vitaminhorse.com.br/produtos/horse-cream-200g-sabor-avela-com-pimenta-e-creatina-vitamin-horse-4si7m/"
   },
   {
-    id: 4,
+    id: 6,
     name: "CREATINA 1KG – PERFORMANCE MÁXIMA E RECUPERAÇÃO - VITAMIN HORSE",
     image: creatina1kgImg,
     price: 127.90,
     url: "https://vitaminhorse.com.br/produtos/creatina-1kg-performance-maxima-e-recuperacao-vitamin-horse-evdhp/"
   },
   {
-    id: 5,
+    id: 7,
     name: "CREATINA 150G – POTÊNCIA MÁXIMA NOS TREINOS - VITAMIN HORSE",
     image: creatina150Img,
     price: 47.90,
     url: "https://vitaminhorse.com.br/produtos/creatina-150g-potencia-maxima-nos-treinos-vitamin-horse-si3d0/"
   },
   {
-    id: 6,
+    id: 8,
     name: "CARNE HORSE PROTEÍNA BOVINA + CREATINA + PEPCOL E L-ARGININA 900G",
     image: carneHorse,
     price: 217.90,
     url: "https://vitaminhorse.com.br/produtos/carne-horse-proteina-bovina-pepcol-e-l-arginina-900g/"
   },
   {
-    id: 7,
-    name: "CREAHORSE 90 CÁPSULAS – SUPERE SEUS LIMITES - VITAMIN HORSE",
-    image: creaHorse,
-    price: 37.90,
-    url: "https://vitaminhorse.com.br/produtos/creahorse-90-capsulas-supere-seus-limites-vitamin-horse-jf23g/"
-  },
-  {
-    id: 8,
-    name: "PUMONEW WHEY BAR ENERGY SABOR FRUTAS VERMELHAS",
-    image: wheyBar,
-    price: 132.00,
-    url: "https://vitaminhorse.com.br/produtos/pumonew-whey-bar-energy-sabor-frutas-vermelhas/"
+    id: 9,
+    name: "NOVO PUMONEW EPHEDRA BLACK 300g",
+    image: pumonewEB,
+    price: 189.90,
+    url: "https://vitaminhorse.com.br/produtos/pre-treino-pumonew-ephedra-black-300g-vitamin-horse/"
   },
 ];
